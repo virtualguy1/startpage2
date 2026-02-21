11c0af240ffc848ca643bb6112959159eb6ac9c2
Abhinav <abhinavrx97@gmail.com>
2026-02-21 14:11:16 +0530

    commit-5 grid glow

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     initClock('clock', 'greeting');
     renderBookmarks('categories-grid', bookmarks);
 });
+
+window.addEventListener('mousemove', (e) => {
+    document.body.style.setProperty('--mouse-x', `${e.clientX}px`);
+    document.body.style.setProperty('--mouse-y', `${e.clientY}px`);
+});
